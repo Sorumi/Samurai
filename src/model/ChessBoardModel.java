a06d4d63d45f143359777c9254cadc4fa33fd58e
宋 奎熹 <1056245944@qq.com>
2016-03-25 22:35:40 +0800

    no message

--- src/model/ChessBoardModel.java
+++ src/model/ChessBoardModel.java
@@ -1,5 +1,11 @@
 package model;
 
 public class ChessBoardModel extends BaseModel {
+    private int length;
+    private int[][] chessBoardBlock;
 
+    public ChessBoardModel(int length){
+        this.length = length;
+        this.chessBoardBlock = new int[this.length][this.length];
+    }
 }
--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -1,5 +1,9 @@
 package model;
 
 public class GameModel extends BaseModel {
+    private ChessBoardModel chessBoardModel;
 
+    public GameModel(){
+        this.chessBoardModel = new ChessBoardModel(15);
+    }
 }
