56eec57ad78134925387529e14b00476c0a65fe2
宋 奎熹 <1056245944@qq.com>
2016-03-26 20:48:59 +0800

    no message

--- src/model/GameModel.java
+++ src/model/GameModel.java
@@ -1,10 +1,30 @@
 package model;
 
+import model.state.GameResultState;
+import model.state.GameState;
+
 public class GameModel extends BaseModel {
     private ChessBoardModel chessBoardModel;
-//    private
+    private GameState gameState;
+    private GameResultState gameResultState;
+    private int currentSamurai;
+//    private Player[] players
+    private int round;
 
     public GameModel(){
         this.chessBoardModel = new ChessBoardModel(15);
+        this.gameState = GameState.RUN;
+        this.currentSamurai = 0;
+        this.round = 1008;
+    }
+
+    public boolean gameStart(){
+
+        return true;
+    }
+
+    public boolean gameOver(){
+
+        return true;
     }
 }
---
+++ src/model/state/GameResultState.java
@@ -0,0 +1,7 @@
+package model.state;
+
+/**
+ * Created by Kray on 16/3/26.
+ */
+public class GameResultState {
+}
