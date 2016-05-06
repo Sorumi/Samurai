f394f9940ab5a3c043f8534d3cbb87d588e91515
Silver Narcissus <827011867@qq.com>
2016-05-06 16:08:35 +0800

    查找全部可用材料的方法完成！

---
+++ src/model/po/Material.java
@@ -0,0 +1,27 @@
+package model.po;
+
+public class Material {
+	private int type;
+	private int number;
+
+	public Material(int type, int number) {
+		this.type = type;
+		this.number = number;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public int getNumer() {
+		return number;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+}
