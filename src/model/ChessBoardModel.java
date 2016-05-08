253978a99f2af367e5bd29256d35b0db106d463f
Silver Narcissus <827011867@qq.com>
2016-05-09 00:58:29 +0800

    完善AI  修改几个bug

--- src/model/ChessBoardModel.java
+++ src/model/ChessBoardModel.java
@@ -8,36 +8,41 @@ public class ChessBoardModel extends BaseModel implements Cloneable {
 
 	public ChessBoardModel(int length) {
 		this.length = length;
-		actualBlockArray = new ActualBlock[this.length+1][this.length+1];
+		actualBlockArray = new ActualBlock[this.length + 1][this.length + 1];
 		for (int x = 0; x <= this.length; x++) {
 			for (int y = 0; y <= this.length; y++) {
 				this.actualBlockArray[x][y] = new ActualBlock(x, y);
 			}
 		}
 	}
-	public ChessBoardModel(int length,ActualBlock[][] actualBlocks) {
-		this.length=length;
-		actualBlockArray=actualBlocks;
+
+	public ChessBoardModel(int length, ActualBlock[][] actualBlocks) {
+		this.length = length;
+		actualBlockArray = actualBlocks;
 	}
 
-	public ActualBlock getActualBlock(int x, int y){
+	public ActualBlock getActualBlock(int x, int y) {
 		return actualBlockArray[x][y];
+	}
+
+	public int getLength() {
+		return length;
 	}
 
 	public void changeActualBlock(int x, int y, int state) {
 		this.actualBlockArray[x][y].setState(state);
-		super.updateChange(new UpdateMessage("block",actualBlockArray[x][y]));
+		super.updateChange(new UpdateMessage("block", actualBlockArray[x][y]));
 	}
 
 	public void changeActualBlock(int x, int y, boolean occ) {
 		actualBlockArray[x][y].setOccupied(occ);
 	}
 
-	public void setActualBlockVisible(int x, int y, boolean visible){
+	public void setActualBlockVisible(int x, int y, boolean visible) {
 		actualBlockArray[x][y].setVisible(visible);
 	}
 
-	public boolean getActualBlockVisible(int x, int y){
+	public boolean getActualBlockVisible(int x, int y) {
 		return actualBlockArray[x][y].getVisible();
 	}
 
@@ -49,28 +54,27 @@ public class ChessBoardModel extends BaseModel implements Cloneable {
 		return this.actualBlockArray[x][y].getOccupied();
 	}
 
-	public int[] getStatesOfAllBlocks(){
-		//数组第几号元素(1~6)对应被几号武士占领,调用时候是[1...6]
+	public int[] getStatesOfAllBlocks() {
+		// 数组第几号元素(1~6)对应被几号武士占领,调用时候是[1...6]
 		int[] statesCount = new int[7];
 		for (int x = 0; x <= this.length; x++) {
 			for (int y = 0; y <= this.length; y++) {
-				statesCount[getActualBlockState(x,y)]++;
+				statesCount[getActualBlockState(x, y)]++;
 			}
 		}
 		return statesCount;
 	}
 
 	public ChessBoardModel clone() {
-		try{
-			ChessBoardModel chessBoardModel=new ChessBoardModel(length, new ActualBlock[length+1][length+1]);
-			for(int i=0;i<this.actualBlockArray.length;i++){
-				for(int x=0;x<this.actualBlockArray.length;x++){
-				chessBoardModel.actualBlockArray[i][x]=this.actualBlockArray[i][x].clone();
+		try {
+			ChessBoardModel chessBoardModel = new ChessBoardModel(length, new ActualBlock[length + 1][length + 1]);
+			for (int i = 0; i < this.actualBlockArray.length; i++) {
+				for (int x = 0; x < this.actualBlockArray.length; x++) {
+					chessBoardModel.actualBlockArray[i][x] = this.actualBlockArray[i][x].clone();
 				}
 			}
 			return chessBoardModel;
-		}
-		catch(Exception ex){
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			return null;
 		}
