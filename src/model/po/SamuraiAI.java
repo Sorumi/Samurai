b4fc8a734324bc65deb1449f2239cf67cfddf6a2
Silver Narcissus <827011867@qq.com>
2016-05-09 20:57:49 +0800

    测试

--- src/model/po/SamuraiAI.java
+++ src/model/po/SamuraiAI.java
@@ -92,6 +92,9 @@ public class SamuraiAI {
 
 	public ArrayList<ActionOperation> storyCalculate(SamuraiPO enemySamuraiPO, Position aidPosition) {
 		ArrayList<ActionOperation> actionOperations = new ArrayList<ActionOperation>();
+		if(enemySamuraiPO!=null){
+			System.out.println("enemy location"+enemySamuraiPO.getPos().getX()+" "+enemySamuraiPO.getPos().getY());
+		}
 		Random random = new Random();
 		samuraiPO.setActionPoint(10);
 		SamuraiPO samuraiPOClone = samuraiPO.clone();
@@ -773,7 +776,7 @@ public class SamuraiAI {
 		if (samuraiPO.getHealthPoint() >= enemySamuraiPO.getHealthPoint()) {
 			count++;
 		}
-		if (count >= 5) {
+		if (count >= 0) {
 			return true;
 		} else {
 			return false;
