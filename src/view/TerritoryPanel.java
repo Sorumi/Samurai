bbe339972d4f23baa62ba4247a1f93d6a71374b2
KrayC <1056245944@qq.com>
2016-05-11 19:23:34 +0800

    tP 写了个的 set6Properties

--- src/view/TerritoryPanel.java
+++ src/view/TerritoryPanel.java
@@ -189,9 +189,7 @@ public class TerritoryPanel extends Pane {
 		weatherPanel.setLayoutY(445);
 		territoryGroup.getChildren().add(weatherPanel);
 
-		this.samuraiProperties_1 = this.territoryHandler.getTerritoryController().get6Properties(1);
-		this.samuraiProperties_2 = this.territoryHandler.getTerritoryController().get6Properties(2);
-		this.samuraiProperties_3 = this.territoryHandler.getTerritoryController().get6Properties(3);
+		this.set6Properties();
 
 		//blur
 		blur = new GaussianBlur(0);
@@ -255,6 +253,12 @@ public class TerritoryPanel extends Pane {
 			default:
 				return new int[]{0};
 		}
+	}
+
+	public void set6Properties(){
+		this.samuraiProperties_1 = this.territoryHandler.getTerritoryController().get6Properties(1);
+		this.samuraiProperties_2 = this.territoryHandler.getTerritoryController().get6Properties(2);
+		this.samuraiProperties_3 = this.territoryHandler.getTerritoryController().get6Properties(3);
 	}
 
 	public TerritoryHandler getTerritoryHandler(){
