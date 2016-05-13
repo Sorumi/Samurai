4627c80848e267371f89c349aa91d644f6e84fd1
Silver Narcissus <827011867@qq.com>
2016-05-13 20:19:05 +0800

    计分板OK

--- src/controller/MapController.java
+++ src/controller/MapController.java
@@ -31,7 +31,7 @@ public class MapController extends TerritoryController {
 
     public void startGameAtLevel(int level){
 
-        this.gameModel = new GameModel(24, 14, gamePanel, level, this.storyModel.getSamuraiPOs());
+        this.gameModel = new GameModel(4, 14, gamePanel, level, this.storyModel.getSamuraiPOs());
         this.gameModel.addObserver(this.gamePanel);
         this.gameModel.getChessBoardModel().addObserver(this.gamePanel);
 
--- src/model/po/PropsStore.java
+++ src/model/po/PropsStore.java
@@ -8,26 +8,55 @@ public class PropsStore {
 	private void useHPjar(int level, SamuraiPO samuraiPO) {
 		switch (level) {
 		case 1:
-			if (samuraiPO.getHealthPoint() + 30 > samuraiPO.getTotalHealthPoint()) {
+			if (samuraiPO.getHealthPoint() + 30 <= samuraiPO.getTotalHealthPoint()) {
 				samuraiPO.setHealthPoint(samuraiPO.getHealthPoint() + 30);
 			} else {
 				samuraiPO.setHealthPoint(samuraiPO.getTotalHealthPoint());
 			}
 			break;
 		case 2:
-			if (samuraiPO.getHealthPoint() + 80 > samuraiPO.getTotalHealthPoint()) {
+			if (samuraiPO.getHealthPoint() + 80 <= samuraiPO.getTotalHealthPoint()) {
 				samuraiPO.setHealthPoint(samuraiPO.getHealthPoint() + 80);
 			} else {
 				samuraiPO.setHealthPoint(samuraiPO.getTotalHealthPoint());
 			}
 			break;
 		case 3:
-			if (samuraiPO.getHealthPoint() + 150 > samuraiPO.getTotalHealthPoint()) {
+			if (samuraiPO.getHealthPoint() + 150 <= samuraiPO.getTotalHealthPoint()) {
 				samuraiPO.setHealthPoint(samuraiPO.getHealthPoint() + 150);
 			} else {
 				samuraiPO.setHealthPoint(samuraiPO.getTotalHealthPoint());
 			}
 
+			break;
+
+		default:
+			System.out.println("Props wrong!");
+			break;
+		}
+	}
+	private void useAPjar(int level, SamuraiPO samuraiPO) {
+		switch (level) {
+		case 1:
+			if (samuraiPO.getActionPoint() + 4 <= samuraiPO.getTotalActionPoint()) {
+				samuraiPO.setActionPoint(samuraiPO.getActionPoint() + 4);
+			} else {
+				samuraiPO.setActionPoint(samuraiPO.getTotalActionPoint());
+			}
+			break;
+		case 2:
+			if (samuraiPO.getActionPoint() + 8 <= samuraiPO.getTotalActionPoint()) {
+				samuraiPO.setActionPoint(samuraiPO.getActionPoint() + 8);
+			} else {
+				samuraiPO.setActionPoint(samuraiPO.getTotalActionPoint());
+			}
+			break;
+		case 3:
+			if (samuraiPO.getActionPoint() + 15 <= samuraiPO.getTotalActionPoint()) {
+				samuraiPO.setActionPoint(samuraiPO.getActionPoint() + 15);
+			} else {
+				samuraiPO.setActionPoint(samuraiPO.getTotalActionPoint());
+			}
 			break;
 
 		default:
