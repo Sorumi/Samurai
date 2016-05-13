0bcab683ca8c0d5c203d33f9817bba3840fed59e
Silver Narcissus <827011867@qq.com>
2016-05-13 20:00:58 +0800

    材料商店雏形

---
+++ src/model/po/Props.java
@@ -0,0 +1,18 @@
+package model.po;
+
+public class Props {
+	private int type;
+	private int number;
+
+	public int getType() {
+		return type;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void changeNumber(int number) {
+		this.number = this.number + number;
+	}
+}
