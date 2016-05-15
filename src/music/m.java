6b86836bb236d7f3fecc14d4fa1da58861706526
Vivian <1030518209@qq.com>
2016-05-15 16:05:25 +0800

    加了一个SmithyWrapper，但是是假的，等帖学姐把图画好就重新加

--- src/music/m.java
+++
@@ -1,5 +0,0 @@
-package music;
-
-public class m {
-
-}
---
+++ src/view/smithy/SmithyItemWrapper5.java
@@ -0,0 +1,51 @@
+package view.smithy;
+
+import javafx.collections.FXCollections;
+import view.eventhandler.SmithyHandler;
+
+public class SmithyItemWrapper5 extends SmithyItemWrapper {
+	public SmithyItemWrapper5(SmithyHandler smithyHandler) {
+		super(3, smithyHandler);
+
+		SmithyItemView item311 = new SmithyItemView(311, -1);
+		item311.setLayoutY(300);
+
+		SmithyItemView item312 = new SmithyItemView(312, 0);
+		item312.setLayoutY(300);
+
+		SmithyItemView item313 = new SmithyItemView(313, -200);
+		item313.setLayoutY(100);
+
+		SmithyItemView item314 = new SmithyItemView(314, 0);
+		item314.setLayoutY(100);
+
+		SmithyItemView item315 = new SmithyItemView(315, 0);
+		item315.setLayoutY(100);
+
+		SmithyItemView item316 = new SmithyItemView(316, 0);
+		item316.setLayoutY(100);
+
+		SmithyItemView item323 = new SmithyItemView(323, 200);
+		item323.setLayoutY(500);
+
+		SmithyItemView item324 = new SmithyItemView(324, 0);
+		item324.setLayoutY(500);
+
+		SmithyItemView item325 = new SmithyItemView(325, 0);
+		item325.setLayoutY(500);
+
+		SmithyItemView item326 = new SmithyItemView(326, 0);
+		item326.setLayoutY(500);
+
+		// TODO
+		list = FXCollections.observableArrayList(item311, item312, item313, item314, item315, item316, item323, item324,
+				item325, item326);
+
+		// this.buildGroup.getChildren().addAll();
+		this.lockGroup.getChildren().addAll(item311, item312, item313, item314, item315, item316, item323, item324,
+				item325, item326);
+
+		this.initWeaponView();
+	}
+
+}
